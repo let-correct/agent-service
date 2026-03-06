08e4df7f064658675fd16892d31bb1f36a8a8798
Troy Snowden <troybradsnowden@gmail.com>
2026-03-06 11:02:31 +0000

    feat: implement arthur oauth2 flow

--- internal/domain/oauth2/token_repository.go
+++ internal/domain/oauth2/token_repository.go
@@ -2,7 +2,7 @@ package oauth2
 
 import "context"
 
-type Repository interface {
+type TokenRepository interface {
 	Save(ctx context.Context, token *Token) error
 	FindByEmailAndProvider(ctx context.Context, email string, provider ProviderID) (*Token, error)
 }
